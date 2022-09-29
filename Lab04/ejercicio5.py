897a8f53cdecd68b270d62a977d2e4d612a47fee
danielpreyes <112120270+danielpreyes@users.noreply.github.com>
2022-09-29 00:01:00 -0600

    Add files via upload

---
+++ Lab04/ejercicio5.py
@@ -0,0 +1,17 @@
+def calcularSalario(horas, tarifa):
+    HORAS_SEMANALES = 40
+    horas_extras = 0
+    if (horas > HORAS_SEMANALES):
+        horas_extras = horas - HORAS_SEMANALES
+        calculo = (HORAS_SEMANALES * tarifa) + (horas_extras * (tarifa * 1.5))
+    else:
+      calculo = (horas * tarifa)   
+    return calculo 
+    
+try:
+    horas = int(input("Ingrese número de horas trabajadas: "))
+    tarifa = float(input("Ingrese tarifa por hora: "))
+    salario = calcularSalario(horas, tarifa)
+    print(salario)
+except:
+    print("Error, ingresar un valor numérico")
